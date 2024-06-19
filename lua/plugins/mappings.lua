bba49823df43f30ad423ed68d302723af3701ca5
Jun Villa <jun@aucto.com>
2024-06-19 15:29:32 -0400

    initial commit

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<C-p>"] = { function() require("telescope.builtin").find_files() end, desc = "Go to file" },
+          ["<F1>"] = { ":ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+          ["<F4>"] = { ":TermExec cmd='npm run dev'<cr>", desc = "npm run dev" },
+          ["gt"] = {
+            function() require("astrocore.buffer").nav(vim.v.count1) end,
+            desc = "Next tab",
+          },
+          ["gT"] = {
+            function() require("astrocore.buffer").nav(-vim.v.count1) end,
+            desc = "Previous tab",
+          },
+          ["H"] = { "^", desc = "beginning of line" },
+          ["L"] = { "$", desc = "end of line" },
+          ["J"] = { "<C-e><C-e>", desc = "Scroll screen down" },
+          ["K"] = { "<C-y><C-y>", desc = "Scroll screen up" },
+          ["Q"] = { "@q", desc = "Quick execute macro" },
+          ["<tab>"] = { ":normal %<cr>", desc = "Go to matching pair" },
+          [",v"] = { "<C-v>", desc = "Enter visual block mode" },
+          ["<F2>"] = { function() vim.lsp.buf.rename() end, desc = "Rename symbol" },
+
+          -- altered functionality
+          [">"] = { ">>", desc = "Shift line right" },
+          ["<"] = { "<<", desc = "Shift line left" },
+          ["n"] = { "nzz", desc = "Default function + re-center screen" },
+          ["N"] = { "Nzz", desc = "Default function + re-center screen" },
+          ["{"] = { "{zz", desc = "Default function + re-center screen" },
+          ["}"] = { "}zz", desc = "Default function + re-center screen" },
+          ["*"] = { "*zz", desc = "Default function + re-center screen" },
+          ["#"] = { "#zz", desc = "Default function + re-center screen" },
+
+          ["%"] = { "%zz", desc = "Default function + re-center screen" },
+          ["<C-d>"] = { "<C-d>zz", desc = "Default function + re-center screen" },
+        },
+        i = {
+          ["<C-l>"] = { function() require("luasnip").jump(1) end, desc = "Jump to next snippet placeholder" },
+          ["<C-h>"] = { function() require("luasnip").jump(-1) end, desc = "Jump to previous snippet placeholder" },
+        },
+        v = {
+          ["<C-c>"] = { "y", desc = "Copy" },
+          ["<C-v>"] = { "P", desc = "Paste but don't clobber register" },
+        },
+        t = {
+          ["<F1>"] = { "<C-\\><C-n>:ToggleTerm direction=float<cr>", desc = "npm run dev" },
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          ["<leader>k"] = {
+            function() vim.lsp.buf.hover() end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function() vim.lsp.buf.declaration() end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
---
+++ lua/polish.lua
@@ -0,0 +1,30 @@
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+vim.cmd.colorscheme "catppuccin"
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require("telescope.actions").close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then vim.cmd(string.format("%s %s", "edit", j.path)) end
+    end
+  else
+    require("telescope.actions").select_default(prompt_bufnr)
+  end
+end
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<CR>"] = select_one_or_multi,
+      },
+    },
+  },
+}
