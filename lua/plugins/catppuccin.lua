168c2ecfa4d0a40b0b44c9ce74a72253c683b8a5
Jun Villa <jun@aucto.com>
2025-04-23 11:35:40 -0400

    add support for transparent bg

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,20 +3,21 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
+      transparent_background = true,
       dim_inactive = {
         enabled = true,
         shade = "dark",
         percentage = 0.50,
       },
-      highlight_overrides = {
-        mocha = function(c)
-          return {
-            Normal = { bg = c.mantle },
-            Comment = { fg = "#7687a0" },
-            ["@tag.attribute"] = { style = {} },
-          }
-        end,
-      },
+      -- highlight_overrides = {
+      --   mocha = function(c)
+      --     return {
+      --       Normal = { bg = c.mantle },
+      --       Comment = { fg = "#7687a0" },
+      --       ["@tag.attribute"] = { style = {} },
+      --     }
+      --   end,
+      -- },
     },
   },
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -13,7 +13,7 @@ local get_system_colorscheme = function()
 end
 
 if get_system_colorscheme() == "light" then
-  vim.cmd.colorscheme "catppuccin-mocha"
+  vim.cmd.colorscheme "catppuccin-latte"
 else
   vim.cmd.colorscheme "catppuccin-mocha"
 end
@@ -40,28 +40,6 @@ cmp.setup {
   }, {
     { name = "buffer" },
   }),
-}
-
-local select_one_or_multi = function(prompt_bufnr)
-  local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-  local multi = picker:get_multi_selection()
-  if not vim.tbl_isempty(multi) then
-    require("telescope.actions").close(prompt_bufnr)
-    for _, j in pairs(multi) do
-      if j.path ~= nil then vim.cmd(string.format("%s %s", "edit", j.path)) end
-    end
-  else
-    require("telescope.actions").select_default(prompt_bufnr)
-  end
-end
-require("telescope").setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<CR>"] = select_one_or_multi,
-      },
-    },
-  },
 }
 
 vim.api.nvim_del_keymap("n", "gra")
