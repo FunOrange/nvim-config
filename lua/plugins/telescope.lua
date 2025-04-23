51098296d3b622f652c835e117fd250952ae871f
Jun Villa <jun@aucto.com>
2025-04-23 11:32:53 -0400

    make telescope bigger

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    defaults = {
+      layout_config = {
+        horizontal = { width = 0.99, height = 0.99 },
+      },
+    },
+  },
+}
