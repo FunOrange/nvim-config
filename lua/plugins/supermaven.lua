c161771883aa2b95afc185b5e395d612459ee97b
FunOrange <junarvi@yahoo.ca>
2026-01-05 01:02:47 -0500

    fix: no supermaven binary

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,5 +1,6 @@
 return {
   "supermaven-inc/supermaven-nvim",
+  enabled = vim.loop.os_uname().sysname ~= "Windows_NT" and jit.arch ~= "arm64",
   config = function()
     require("supermaven-nvim").setup {
       log_level = "off",
